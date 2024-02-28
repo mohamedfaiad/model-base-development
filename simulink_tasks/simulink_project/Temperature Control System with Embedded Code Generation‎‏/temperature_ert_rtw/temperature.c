fc28cc5448bc96270ed3245fc7431533e735f223
m.faiad <157193071+mohamedfaiad@users.noreply.github.com>
2024-02-28 21:22:17 +0200

    Add files via upload

---
+++ simulink_tasks/simulink_project/Temperature Control System with Embedded Code Generation‎‏/temperature_ert_rtw/temperature.c
@@ -0,0 +1,260 @@
+/*
+ * File: temperature.c
+ *
+ * Code generated for Simulink model 'temperature'.
+ *
+ * Model version                  : 1.3
+ * Simulink Coder version         : 9.5 (R2021a) 14-Nov-2020
+ * C/C++ source code generated on : Wed Feb 28 21:01:32 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "temperature.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmSetFirstInitCond
+#define rtmSetFirstInitCond(rtm, val)  ((rtm)->Timing.firstInitCondFlag = (val))
+#endif
+
+#ifndef rtmIsFirstInitCond
+#define rtmIsFirstInitCond(rtm)        ((rtm)->Timing.firstInitCondFlag)
+#endif
+
+#ifndef rtmIsMajorTimeStep
+#define rtmIsMajorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MAJOR_TIME_STEP)
+#endif
+
+#ifndef rtmIsMinorTimeStep
+#define rtmIsMinorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MINOR_TIME_STEP)
+#endif
+
+#ifndef rtmSetTPtr
+#define rtmSetTPtr(rtm, val)           ((rtm)->Timing.t = (val))
+#endif
+
+/* Continuous states */
+X rtX;
+
+/* Block signals and states (default storage) */
+DW rtDW;
+
+/* Real-time model */
+static RT_MODEL rtM_;
+RT_MODEL *const rtM = &rtM_;
+
+/* private model entry point functions */
+extern void temperature_derivatives(void);
+
+/*
+ * This function updates continuous states using the ODE3 fixed-step
+ * solver algorithm
+ */
+static void rt_ertODEUpdateContinuousStates(RTWSolverInfo *si )
+{
+  /* Solver Matrices */
+  static const real_T rt_ODE3_A[3] = {
+    1.0/2.0, 3.0/4.0, 1.0
+  };
+
+  static const real_T rt_ODE3_B[3][3] = {
+    { 1.0/2.0, 0.0, 0.0 },
+
+    { 0.0, 3.0/4.0, 0.0 },
+
+    { 2.0/9.0, 1.0/3.0, 4.0/9.0 }
+  };
+
+  time_T t = rtsiGetT(si);
+  time_T tnew = rtsiGetSolverStopTime(si);
+  time_T h = rtsiGetStepSize(si);
+  real_T *x = rtsiGetContStates(si);
+  ODE3_IntgData *id = (ODE3_IntgData *)rtsiGetSolverData(si);
+  real_T *y = id->y;
+  real_T *f0 = id->f[0];
+  real_T *f1 = id->f[1];
+  real_T *f2 = id->f[2];
+  real_T hB[3];
+  int_T i;
+  int_T nXc = 1;
+  rtsiSetSimTimeStep(si,MINOR_TIME_STEP);
+
+  /* Save the state values at time t in y, we'll use x as ynew. */
+  (void) memcpy(y, x,
+                (uint_T)nXc*sizeof(real_T));
+
+  /* Assumes that rtsiSetT and ModelOutputs are up-to-date */
+  /* f0 = f(t,y) */
+  rtsiSetdX(si, f0);
+  temperature_derivatives();
+
+  /* f(:,2) = feval(odefile, t + hA(1), y + f*hB(:,1), args(:)(*)); */
+  hB[0] = h * rt_ODE3_B[0][0];
+  for (i = 0; i < nXc; i++) {
+    x[i] = y[i] + (f0[i]*hB[0]);
+  }
+
+  rtsiSetT(si, t + h*rt_ODE3_A[0]);
+  rtsiSetdX(si, f1);
+  temperature_step();
+  temperature_derivatives();
+
+  /* f(:,3) = feval(odefile, t + hA(2), y + f*hB(:,2), args(:)(*)); */
+  for (i = 0; i <= 1; i++) {
+    hB[i] = h * rt_ODE3_B[1][i];
+  }
+
+  for (i = 0; i < nXc; i++) {
+    x[i] = y[i] + (f0[i]*hB[0] + f1[i]*hB[1]);
+  }
+
+  rtsiSetT(si, t + h*rt_ODE3_A[1]);
+  rtsiSetdX(si, f2);
+  temperature_step();
+  temperature_derivatives();
+
+  /* tnew = t + hA(3);
+     ynew = y + f*hB(:,3); */
+  for (i = 0; i <= 2; i++) {
+    hB[i] = h * rt_ODE3_B[2][i];
+  }
+
+  for (i = 0; i < nXc; i++) {
+    x[i] = y[i] + (f0[i]*hB[0] + f1[i]*hB[1] + f2[i]*hB[2]);
+  }
+
+  rtsiSetT(si, tnew);
+  rtsiSetSimTimeStep(si,MAJOR_TIME_STEP);
+}
+
+/* Model step function */
+void temperature_step(void)
+{
+  int32_T tmp;
+  if (rtmIsMajorTimeStep(rtM)) {
+    /* set solver stop time */
+    rtsiSetSolverStopTime(&rtM->solverInfo,((rtM->Timing.clockTick0+1)*
+      rtM->Timing.stepSize0));
+  }                                    /* end MajorTimeStep */
+
+  /* Update absolute time of base rate at minor time step */
+  if (rtmIsMinorTimeStep(rtM)) {
+    rtM->Timing.t[0] = rtsiGetT(&rtM->solverInfo);
+  }
+
+  /* Integrator: '<Root>/Integrator' */
+  if (rtDW.Integrator_IWORK != 0) {
+    rtX.Integrator_CSTATE = 0.5;
+  }
+
+  /* Step: '<Root>/set point temperature' */
+  if (rtM->Timing.t[0] < 2.0) {
+    tmp = 10;
+  } else {
+    tmp = 20;
+  }
+
+  /* End of Step: '<Root>/set point temperature' */
+
+  /* Sum: '<Root>/Sum' incorporates:
+   *  Integrator: '<Root>/Integrator'
+   */
+  rtDW.Sum = (real_T)tmp - rtX.Integrator_CSTATE;
+  if (rtmIsMajorTimeStep(rtM)) {
+    /* Update for Integrator: '<Root>/Integrator' */
+    rtDW.Integrator_IWORK = 0;
+  }                                    /* end MajorTimeStep */
+
+  if (rtmIsMajorTimeStep(rtM)) {
+    rt_ertODEUpdateContinuousStates(&rtM->solverInfo);
+
+    /* Update absolute time for base rate */
+    /* The "clockTick0" counts the number of times the code of this task has
+     * been executed. The absolute time is the multiplication of "clockTick0"
+     * and "Timing.stepSize0". Size of "clockTick0" ensures timer will not
+     * overflow during the application lifespan selected.
+     */
+    ++rtM->Timing.clockTick0;
+    rtM->Timing.t[0] = rtsiGetSolverStopTime(&rtM->solverInfo);
+
+    {
+      /* Update absolute timer for sample time: [0.01s, 0.0s] */
+      /* The "clockTick1" counts the number of times the code of this task has
+       * been executed. The resolution of this integer timer is 0.01, which is the step size
+       * of the task. Size of "clockTick1" ensures timer will not overflow during the
+       * application lifespan selected.
+       */
+      rtM->Timing.clockTick1++;
+    }
+  }                                    /* end MajorTimeStep */
+}
+
+/* Derivatives for root system: '<Root>' */
+void temperature_derivatives(void)
+{
+  XDot *_rtXdot;
+  _rtXdot = ((XDot *) rtM->derivs);
+
+  /* Derivatives for Integrator: '<Root>/Integrator' */
+  _rtXdot->Integrator_CSTATE = rtDW.Sum;
+}
+
+/* Model initialize function */
+void temperature_initialize(void)
+{
+  /* Registration code */
+  {
+    /* Setup solver object */
+    rtsiSetSimTimeStepPtr(&rtM->solverInfo, &rtM->Timing.simTimeStep);
+    rtsiSetTPtr(&rtM->solverInfo, &rtmGetTPtr(rtM));
+    rtsiSetStepSizePtr(&rtM->solverInfo, &rtM->Timing.stepSize0);
+    rtsiSetdXPtr(&rtM->solverInfo, &rtM->derivs);
+    rtsiSetContStatesPtr(&rtM->solverInfo, (real_T **) &rtM->contStates);
+    rtsiSetNumContStatesPtr(&rtM->solverInfo, &rtM->Sizes.numContStates);
+    rtsiSetNumPeriodicContStatesPtr(&rtM->solverInfo,
+      &rtM->Sizes.numPeriodicContStates);
+    rtsiSetPeriodicContStateIndicesPtr(&rtM->solverInfo,
+      &rtM->periodicContStateIndices);
+    rtsiSetPeriodicContStateRangesPtr(&rtM->solverInfo,
+      &rtM->periodicContStateRanges);
+    rtsiSetErrorStatusPtr(&rtM->solverInfo, (&rtmGetErrorStatus(rtM)));
+    rtsiSetRTModelPtr(&rtM->solverInfo, rtM);
+  }
+
+  rtsiSetSimTimeStep(&rtM->solverInfo, MAJOR_TIME_STEP);
+  rtM->intgData.y = rtM->odeY;
+  rtM->intgData.f[0] = rtM->odeF[0];
+  rtM->intgData.f[1] = rtM->odeF[1];
+  rtM->intgData.f[2] = rtM->odeF[2];
+  rtM->contStates = ((X *) &rtX);
+  rtsiSetSolverData(&rtM->solverInfo, (void *)&rtM->intgData);
+  rtsiSetSolverName(&rtM->solverInfo,"ode3");
+  rtmSetTPtr(rtM, &rtM->Timing.tArray[0]);
+  rtM->Timing.stepSize0 = 0.01;
+  rtmSetFirstInitCond(rtM, 1);
+
+  /* InitializeConditions for Integrator: '<Root>/Integrator' */
+  if (rtmIsFirstInitCond(rtM)) {
+    rtX.Integrator_CSTATE = 0.5;
+  }
+
+  rtDW.Integrator_IWORK = 1;
+
+  /* End of InitializeConditions for Integrator: '<Root>/Integrator' */
+
+  /* set "at time zero" to false */
+  if (rtmIsFirstInitCond(rtM)) {
+    rtmSetFirstInitCond(rtM, 0);
+  }
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
